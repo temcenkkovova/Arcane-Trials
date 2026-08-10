578373e6557993541531dad46bc0d6c2d4138e98
Temchenko <temcenkovova8@gmail.com>
2026-08-10 16:10:45 +0200

    Create PlayerConfig based on scriptable object

---
+++ Assets/ArcaneTrials/ScriptableObjects/Player/PlayerConfig.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerConfig", menuName = "PlayerConfig", order = 0)]
+public class PlayerConfig : ScriptableObject
+{
+  public float baseSpeed;
+  public float baseSpeedRotation;
+  public float baseHealth;
+  public float baseDamage;
+}
